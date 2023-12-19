2a0139cdbb47c75e94a544a7675fed9757b60df2
Student <Student@Admins-MacBook-Air.local>
2023-12-18 23:57:15 -0500

    add create action

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -1,6 +1,16 @@
 class TripsController < ApplicationController
+  
   def index
     @trips = Trip.all
     render :index
+  end
+
+  def create
+    @trip = Trip.create(
+      title: params[:title],
+      image_url: params[:image_url],
+      user_id: params[:user_id],
+    )
+    render :show
   end
 end
--- test/controllers/trips_controller_test.rb
+++ test/controllers/trips_controller_test.rb
@@ -8,4 +8,11 @@ class TripsControllerTest < ActionDispatch::IntegrationTest
     data = JSON.parse(response.body)
     assert_equal Trip.count, data.length
   end
+
+  test "create" do
+    assert_difference "Trip.count", 1 do
+      post "/trips.json", params: { title: "city", image_url: "test.com", user_id: 1 }
+      assert_response 200
+    end
+  end
 end
