a4ec66da834b1698100de467ecc15e975178d139
Student <Student@Admins-MacBook-Air.local>
2023-12-20 19:50:04 -0500

    add users create action (signup)

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
+
+  #Trip
  get "/trips" => "trips#index"
  post "/trips" => "trips#create"
  get "/trips/:id" => "trips#show"
  patch "/trips/:id" => "trips#update"
  delete "/trips/:id" => "trips#destroy"
+
+ #user
+ post "/users" => "users#create"
+ 
 end
