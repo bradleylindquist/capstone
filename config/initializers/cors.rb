3bad0dfd9c01f4a8b85a30928498477321482c37
Student <Student@Admins-MacBook-Air.local>
2024-01-10 22:17:39 -0500

    fixed cors.rb

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "example.com", "localhost:5173", 'https://brad-travel.onrender.com/'
+    origins "example.com", "localhost:5173", 'brad-travel.onrender.com'
     resource "*", headers: :any, methods: [:get, :post, :patch, :put, :delete]
   end
 end
